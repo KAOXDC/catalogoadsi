bb5bd5851c12e820db34565735da5e8a68459318
Diego Prado <kaoxdc@gmail.com>
2015-03-19 04:26:56 -0500

    statics
    statics

--- catalogo/catalogo/settings.py
+++ catalogo/catalogo/settings.py
@@ -61,12 +61,12 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__),'static/'))
+
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
