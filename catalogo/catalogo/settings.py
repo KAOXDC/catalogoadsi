fd91d5e2a0a1d018b9fc57a062e0050b81e8a813
Diego Prado <kaoxdc@gmail.com>
2015-03-19 04:17:05 -0500

    static storage
    static storage

--- catalogo/catalogo/settings.py
+++ catalogo/catalogo/settings.py
@@ -66,6 +66,7 @@ STATIC_ROOT = ''
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
