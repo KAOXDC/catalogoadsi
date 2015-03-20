65d6b1369b30a864ebb167898ae5173934bafa5c
Diego Prado <kaoxdc@gmail.com>
2015-03-19 21:11:49 -0500

    testing
    testing

--- catalogo/catalogo/settings.py
+++ catalogo/catalogo/settings.py
@@ -61,6 +61,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
+
 STATIC_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__),'static/'))
 
 
